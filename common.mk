5e32e3b8c17492785ded9bd750d8b9e8f8153f2d
Francescodario Cuzzocrea <bosconovic@gmail.com>
2022-03-13 21:17:20 +0000

    exynos9810-common: usb: Switch to hardware/samsung HAL

--- common.mk
+++ common.mk
@@ -273,7 +273,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.1-service.typec
+    android.hardware.usb@1.3-service.samsung
 
 # Soundtrigger
 PRODUCT_PACKAGES += \
